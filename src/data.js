f7efe996073e0da382d2fdb5d48a3e8d576e4033
Chandra <62059587+Chandra-1@users.noreply.github.com>
2022-02-02 03:25:38 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://img.freepik.com/free-photo/perky-girl-stylish-glasses-stares-amazement-walking-pink-wall-brunette-culottes-orange-blouse-posing-with-red-handbag_197531-14254.jpg?size=626&ext=jpg&ga=GA1.1.1519739669.1642982400",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET GLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://media.istockphoto.com/photos/full-length-body-size-side-profile-photo-jumping-high-beautiful-she-picture-id1132930261?k=20&m=1132930261&s=170667a&w=0&h=ktxEilhguTXBhYSpyCP8E3d8eHMmxyG_r-4ti_cTPSo=",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET GLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://expertphotography.b-cdn.net/wp-content/uploads/2018/12/Fashion-model-pose.jpg",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET GLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+];
